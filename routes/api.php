530ef8e43521dd6b824aa3be1c443804db57a997
ikhwanprayoga <yoga.seke@gmail.com>
2018-11-12 22:39:30 +0700

    template

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('tes_sensor', 'SensorController@tes');
+Route::get('tes_sensor_arduino/{id}/{suhu}/{kelembapan}/{asap}', 'SensorController@arduino');
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('nilai', 'SensorController@nilai');
+Route::get('node1', 'SensorController@node1');
+Route::get('node2', 'SensorController@node2');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/beranda', 'BerandaController@index')->name('beranda');
+Route::get('/data', 'DataController@index')->name('data');
+
+Route::group(['prefix' => 'master'], function() {
+	Route::get('kategori-udara', 'MasterKategoriUdaraController@index')->name('master-kategori-udara');
+});
