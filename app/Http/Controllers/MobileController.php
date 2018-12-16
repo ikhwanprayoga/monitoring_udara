238983a9661e8dcb7b7392c949e736e99cd90420
ikhwanprayoga <yoga.seke@gmail.com>
2018-12-16 23:13:13 +0700

    api data monitoring

--- app/Http/Controllers/MobileController.php
+++ app/Http/Controllers/MobileController.php
@@ -10,7 +10,11 @@ class MobileController extends Controller
 {
     public function realtime()
     {
-        $data = Monitoring::all();
+        $data['pm10']       = number_format((float)Monitoring::avg('pm10'), 2, ',', '');
+        $data['co']         = number_format((float)Monitoring::avg('co'), 2, ',', '');
+        $data['asap']       = number_format((float)Monitoring::avg('asap'), 2, ',', '');
+        $data['suhu']       = Monitoring::avg('suhu');
+        $data['kelembapan'] = Monitoring::avg('kelembapan');
 
         return response()->json($data, 200);
     }
